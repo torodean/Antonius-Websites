06b3320912c3e7db165d7e40bc3da632c4951bfb
awtorode@gmail.com <awtorode@gmail.com>
2018-06-20 22:51:03 +0100

    Added to website

--- The Illuminati/contact.php
+++ The Illuminati/contact.php
@@ -2,12 +2,16 @@
 $field_name = $_POST['cf_name'];
 $field_email = $_POST['cf_email'];
 $field_message = $_POST['cf_message'];
+$field_reason = $_POST['cf_reason'];
+$field_found = $_POST['cf_found'];
 
 $mail_to = 'theilluminati.dalaran@gmail.com';
-$subject = 'Message from a site visitor '.$field_name;
+$subject = 'Message from '.$field_name.' regarding '.$field_reason;
 
 $body_message = 'From: '.$field_name."\n";
 $body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Reason: '.$field_reason."\n";
+$body_message .= 'Found us: '.$field_found."\n";
 $body_message .= 'Message: '.$field_message;
 
 $headers = 'From: '.$field_email."\r\n";
@@ -18,14 +22,14 @@ $mail_status = mail($mail_to, $subject, $body_message, $headers);
 if ($mail_status) { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Thank you for the message. We will contact you shortly.');
-		window.location = 'contact_page.html';
+		window.location = 'index.html#contact';
 	</script>
 <?php
 }
 else { ?>
 	<script language="javascript" type="text/javascript">
-		alert('Message failed. Please, send an email to gordon@template-help.com');
-		window.location = 'contact_page.html';
+		alert('Message failed. Please, send an email to theilluminati.dalaran@gmail.com');
+		window.location = 'index.html#contact';
 	</script>
 <?php
 }
